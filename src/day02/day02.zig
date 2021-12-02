9090b10a3c742debec517aaf0c48b3087304aa7d
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-12-02 06:21:08 +0100

    🎅🏽 Complete Day 2 📉

--- src/day02/day02.zig
+++ src/day02/day02.zig
@@ -18,7 +18,22 @@ pub fn main() anyerror!void {
 /// --- Part One ---
 ///
 fn part1() !i32 {
-    return 0;
+    var depth: i32 = 0;
+    var position: i32 = 0;
+    var lines = std.mem.split(u8, std.mem.trimRight(u8, input, "\n"), "\n");
+    while (lines.next()) |line| {
+        var command = std.mem.split(u8, line, " ");
+        var kind = command.next() orelse "";
+        var quantity = try std.fmt.parseInt(i32, command.next() orelse "0", 10);
+        if (std.mem.eql(u8, kind, "up")) {
+            depth -= quantity;
+        } else if (std.mem.eql(u8, kind, "down")) {
+            depth += quantity;
+        } else {
+            position += quantity;
+        }
+    }
+    return depth * position;
 }
 
 test "day02.part1" {
@@ -29,7 +44,24 @@ test "day02.part1" {
 /// --- Part Two ---
 ///
 fn part2() !i32 {
-    return 0;
+    var aim: i32 = 0;
+    var depth: i32 = 0;
+    var position: i32 = 0;
+    var lines = std.mem.split(u8, std.mem.trimRight(u8, input, "\n"), "\n");
+    while (lines.next()) |line| {
+        var command = std.mem.split(u8, line, " ");
+        var kind = command.next() orelse "";
+        var quantity = try std.fmt.parseInt(i32, command.next() orelse "0", 10);
+        if (std.mem.eql(u8, kind, "up")) {
+            aim -= quantity;
+        } else if (std.mem.eql(u8, kind, "down")) {
+            aim += quantity;
+        } else {
+            position += quantity;
+            depth += aim * quantity;
+        }
+    }
+    return depth * position;
 }
 
 test "day02.part2" {
