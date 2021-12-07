59d1aaaf0e6114139ba35da3430a380b35f6f827
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-12-07 06:49:40 +0100

    🧝‍♀️ Complete Part 2 of Day 5 🌀

--- src/day05/day05.zig
+++ src/day05/day05.zig
@@ -22,8 +22,8 @@ pub fn main() anyerror!void {
 ///
 /// --- Part One ---
 ///
-fn part1() !u16 {
-    var overlap: u16 = 0;
+fn part1() !u32 {
+    var overlap: u32 = 0;
     var diagram = [_][size]u16{[_]u16{0} ** size} ** size;
     var lines = tokenize(u8, input, "\r\n");
     while (lines.next()) |line| {
@@ -32,7 +32,7 @@ fn part1() !u16 {
         var y1 = try parseInt(u16, numbers.next() orelse "", 10);
         var x2 = try parseInt(u16, numbers.next() orelse "", 10);
         var y2 = try parseInt(u16, numbers.next() orelse "", 10);
-        overlap += coverLine(&diagram, x1, y1, x2, y2);
+        overlap += coverLine(&diagram, x1, y1, x2, y2, false);
     }
     return overlap;
 }
@@ -45,20 +45,32 @@ test "day05.part1" {
 ///
 /// --- Part Two ---
 ///
-fn part2() !u16 {
-    return 0;
+fn part2() !u32 {
+    var overlap: u32 = 0;
+    var diagram = [_][size]u16{[_]u16{0} ** size} ** size;
+    var lines = tokenize(u8, input, "\r\n");
+    while (lines.next()) |line| {
+        var numbers = tokenize(u8, line, " ,->");
+        var x1 = try parseInt(u16, numbers.next() orelse "", 10);
+        var y1 = try parseInt(u16, numbers.next() orelse "", 10);
+        var x2 = try parseInt(u16, numbers.next() orelse "", 10);
+        var y2 = try parseInt(u16, numbers.next() orelse "", 10);
+        overlap += coverLine(&diagram, x1, y1, x2, y2, true);
+    }
+    return overlap;
 }
 
 test "day05.part2" {
-    try testing.expectEqual(0, comptime try part2());
+    @setEvalBranchQuota(2_000_000); // 🤯🤯🤯
+    try testing.expectEqual(21104, comptime try part2());
 }
 
 ///
-/// coverLine covers all points in the line from (x1, y1) to (x2, y2)
+/// coverLine covers all points in the line (and diagonal, optionally) from (x1, y1) to (x2, y2)
 ///
-fn coverLine(diagram: *[size][size]u16, x1: u16, y1: u16, x2: u16, y2: u16) u16 {
+fn coverLine(diagram: *[size][size]u16, x1: u16, y1: u16, x2: u16, y2: u16, diagonal: bool) u32 {
+    var overlap: u32 = 0;
     if (x1 == x2 or y1 == y2) {
-        var overlap: u16 = 0;
         var x = min(x1, x2);
         while (x <= max(x1, x2)) : (x += 1) {
             var y = min(y1, y2);
@@ -69,7 +81,21 @@ fn coverLine(diagram: *[size][size]u16, x1: u16, y1: u16, x2: u16, y2: u16) u16 
                 }
             }
         }
-        return overlap;
+    } else if (diagonal) {
+        var x = x1;
+        var y = y1;
+        while (true) : ({
+            x = if (x1 < x2) x + 1 else x - 1;
+            y = if (y1 < y2) y + 1 else y - 1;
+        }) {
+            diagram[x][y] += 1;
+            if (diagram[x][y] == 2) {
+                overlap += 1;
+            }
+            if (x == x2 or y == y2) {
+                break;
+            }
+        }
     }
-    return 0;
+    return overlap;
 }
