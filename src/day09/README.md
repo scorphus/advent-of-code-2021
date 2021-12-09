ede4dc203af27960be442e457276ea698b382ef8
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-12-09 07:33:54 +0100

    ‍🎁 Complete Day 9 🛁

--- src/day09/README.md
+++ src/day09/README.md
@@ -1,10 +1,79 @@
-# Advent of Code - Day 9
+## \--- [Day 9](https://adventofcode.com/2021/day/9): Smoke Basin ---
 
-## Part One
+These caves seem to be [lava tubes](https://en.wikipedia.org/wiki/Lava_tube). Parts are even still volcanically active; small hydrothermal vents release smoke into the caves that slowly settles like rain.
 
-TODO: Content
+If you can model how the smoke flows through the caves, you might be able to avoid it and be that much safer. The submarine generates a heightmap of the floor of the nearby caves for you (your puzzle input).
 
-## Part Two
+Smoke flows to the lowest point of the area it's in. For example, consider the following heightmap:
 
-TODO: Content
+<pre><code>2<u>1</u>9994321<u>0</u>
+3987894921
+98<u>5</u>6789892
+8767896789
+989996<u>5</u>678
+</code></pre>
 
+Each number corresponds to the height of a particular location, where `9` is the highest and `0` is the lowest a location can be.
+
+Your first goal is to find the _low points_ - the locations that are lower than any of its adjacent locations. Most locations have four adjacent locations (up, down, left, and right); locations on the edge or corner of the map have three or two adjacent locations, respectively. (Diagonal locations do not count as adjacent.)
+
+In the above example, there are _four_ low points, all highlighted: two are in the first row (a `1` and a `0`), one is in the third row (a `5`), and one is in the bottom row (also a `5`). All other locations on the heightmap have some lower adjacent location, and so are not low points.
+
+The _risk level_ of a low point is _1 plus its height_. In the above example, the risk levels of the low points are `2`, `1`, `6`, and `6`. The sum of the risk levels of all low points in the heightmap is therefore _`15`_.
+
+Find all of the low points on your heightmap. _What is the sum of the risk levels of all low points on your heightmap?_
+
+Your puzzle answer was `537`.
+
+## \--- Part Two ---
+
+Next, you need to find the largest basins so you know what areas are most important to avoid.
+
+A _basin_ is all locations that eventually flow downward to a single low point. Therefore, every low point has a basin, although some basins are very small. Locations of height `9` do not count as being in any basin, and all other locations will always be part of exactly one basin.
+
+The _size_ of a basin is the number of locations within the basin, including the low point. The example above has four basins.
+
+The top-left basin, size `3`:
+
+<p></p>
+<pre><code><u>21</u>99943210
+<u>3</u>987894921
+9856789892
+8767896789
+9899965678
+</code></pre>
+
+The top-right basin, size `9`:
+
+<pre><code>21999<u>43210</u>
+398789<u>4</u>9<u>21</u>
+985678989<u>2</u>
+8767896789
+9899965678
+</code></pre>
+
+The middle basin, size `14`:
+
+<pre><code>2199943210
+39<u>878</u>94921
+9<u>85678</u>9892
+<u>87678</u>96789
+9<u>8</u>99965678
+</code></pre>
+
+The bottom-right basin, size `9`:
+
+<pre><code>2199943210
+3987894921
+9856789<u>8</u>92
+876789<u>678</u>9
+98999<u>65678</u>
+</code></pre>
+
+Find the three largest basins and multiply their sizes together. In the above example, this is _`9 * 14 * 9 = 1134`_.
+
+_What do you get if you multiply together the sizes of the three largest basins?_
+
+Your puzzle answer was `1142757`.
+
+Both parts of this puzzle are complete! They provide two gold stars: \*\*
--- src/day09/day09.zig
+++ src/day09/day09.zig
@@ -1,10 +1,13 @@
 /// # Advent of Code - Day 9
 const std = @import("std");
-const testing = std.testing;
+const parseInt = std.fmt.parseInt;
 const print = std.debug.print;
-const Allocator = std.mem.Allocator;
+const tokenize = std.mem.tokenize;
+const testing = std.testing;
 
 const input = @embedFile("./input.txt");
+const rows = 100;
+const cols = 100;
 
 pub fn main() anyerror!void {
     print("--- Part One ---\n", .{});
@@ -17,21 +20,98 @@ pub fn main() anyerror!void {
 ///
 /// --- Part One ---
 ///
-fn part1() !i32 {
-    return 0;
+fn part1() !u32 {
+    var ans: u32 = 0;
+    var heatmap: [rows][cols]u8 = try parseHeatmap();
+    for (heatmap) |row, i| {
+        for (heatmap[i]) |cell, j| {
+            if (j != 0 and cell >= heatmap[i][j - 1]) continue;
+            if (j != row.len - 1 and cell >= heatmap[i][j + 1]) continue;
+            if (i != 0 and cell >= heatmap[i - 1][j]) continue;
+            if (i != heatmap.len - 1 and cell >= heatmap[i + 1][j]) continue;
+            ans += cell + 1;
+        }
+    }
+    return ans;
 }
 
 test "day09.part1" {
-    try testing.expectEqual(0, comptime try part1());
+    @setEvalBranchQuota(200_000);
+    try testing.expectEqual(537, comptime try part1());
 }
 
 ///
 /// --- Part Two ---
 ///
-fn part2() !i32 {
-    return 0;
+fn part2() !u32 {
+    var s1: u32 = 0;
+    var s2: u32 = 0;
+    var s3: u32 = 0;
+    var heatmap: [rows][cols]u8 = try parseHeatmap();
+    for (heatmap) |row, i| {
+        for (heatmap[i]) |cell, j| {
+            if (j != 0 and cell >= heatmap[i][j - 1]) continue;
+            if (j != row.len - 1 and cell >= heatmap[i][j + 1]) continue;
+            if (i != 0 and cell >= heatmap[i - 1][j]) continue;
+            if (i != heatmap.len - 1 and cell >= heatmap[i + 1][j]) continue;
+            var size = fillBasin(&heatmap, .{ i, j });
+            if (size > s1) {
+                s3 = s2;
+                s2 = s1;
+                s1 = size;
+            } else if (size > s2) {
+                s3 = s2;
+                s2 = size;
+            } else if (size > s3) {
+                s3 = size;
+            }
+        }
+    }
+    return s1 * s2 * s3;
 }
 
 test "day09.part2" {
-    try testing.expectEqual(0, comptime try part2());
+    @setEvalBranchQuota(1_000_000);
+    try testing.expectEqual(1142757, comptime try part2());
+}
+
+///
+/// parseHeatmap parses the input into a 2D array of u8s
+///
+fn parseHeatmap() ![rows][cols]u8 {
+    var heatmap: [rows][cols]u8 = undefined;
+    var input_iter = tokenize(u8, input, "\n");
+    var i: usize = 0;
+    while (input_iter.next()) |row| : (i += 1) {
+        for (row) |cell, j| {
+            heatmap[i][j] = cell - '0';
+        }
+    }
+    return heatmap;
+}
+
+///
+/// fillBasin fills a basin of the start cell in a BFS fashion and returns its size
+///
+fn fillBasin(heatmap: *[rows][cols]u8, start: anytype) u32 {
+    var queue: [rows * cols][2]usize = undefined;
+    var front: u32 = 0;
+    var back: u32 = 1;
+    queue[front] = start;
+    while (front < back) : (front += 1) {
+        var i = queue[front][0];
+        var j = queue[front][1];
+        inline for (.{ .{ 1, 0 }, .{ 0, 1 }, .{ -1, 0 }, .{ 0, -1 } }) |deltas| {
+            if ((i != 0 or deltas[0] != -1) and (j != 0 or deltas[1] != -1)) {
+                var x: usize = @intCast(usize, @intCast(i16, i) + deltas[0]);
+                var y: usize = @intCast(usize, @intCast(i16, j) + deltas[1]);
+                if (x < rows and y < cols and heatmap[x][y] < 9) {
+                    heatmap[x][y] = 9;
+                    queue[back] = .{ x, y };
+                    back += 1;
+                }
+            }
+        }
+    }
+    return back - 1;
 }
